94c78754af708746765287ef839eb7676133b63f
Jerem2360 <jlefoulon@hotmail.com>
2023-11-28 21:23:12 +0100

    restarted from scratch

--- A2-POO-Prosit6.cpp
+++ A2-POO-Prosit6.cpp
@@ -1,12 +1,44 @@
 #include "pch.h"
 #include "MyForm.h"
+#include "Password.h"
+
 using namespace System;
 using namespace System::Windows::Forms;
+using namespace MySql::Data::MySqlClient;
+
+
+const char* user = "root";
+const char* ip = "127.0.0.1";
+const char* db_name = "a2_poo_prosit6";
 
 
 [STAThreadAttribute]
 int main(array<String^>^ args)
 {
+	/*String^ connect_string = gcnew String("server=");
+	connect_string = String::Concat(connect_string, gcnew String(ip));
+	connect_string = String::Concat(connect_string, gcnew String(";uid="));
+	connect_string = String::Concat(connect_string, gcnew String(user));
+	connect_string = String::Concat(connect_string, gcnew String(";pwd="));
+	connect_string = String::Concat(connect_string, gcnew String(password));
+	connect_string = String::Concat(connect_string, gcnew String(";database="));
+	connect_string = String::Concat(connect_string, gcnew String(db_name));
+
+
+	MySqlConnection^ conn = gcnew MySqlConnection();
+	conn->ConnectionString = connect_string;
+	conn->Open();
+
+	MySqlCommand^ cmd = conn->CreateCommand();
+
+	cmd->CommandText = "select * from exempleCpp";
+	cmd->Prepare();
+	MySqlDataReader^ reader = cmd->ExecuteReader();
+	reader->Read();
+	Console::WriteLine(reader);
+
+	conn->Close();*/
+
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	P6new::MyForm monFormulaire;
